bd169e0a5cf22ec5845f52966a0bce19f65030ad
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-11-03 12:26:12 +0100

    Update jsecret.go

--- jsecret.go
+++ jsecret.go
@@ -2,33 +2,30 @@ package main
 
 import (
 	"bufio"
-//	"fmt"
-//	"log"
 	"os"
 	"sync"
-//	"time"
 )
 
 var HashList = []string{}
-var maxConcurrentRequests = 5
-var wg sync.WaitGroup
 
 func main() {
-	semaphore := make(chan struct{}, maxConcurrentRequests)
+	var wg sync.WaitGroup
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
+
 		line := scanner.Text()
-		if isUrl(line) {
+		if isUrl(line) == true {
 			wg.Add(1)
-			go func(url string) {
+			go func() {
 				defer wg.Done()
-				semaphore <- struct{}{}
-				defer func() { <-semaphore }()
-				matcher(url)
-			}(line)
+				matcher(line)
+			}()
+
 		}
+
 	}
 
 	wg.Wait()
+
 }
