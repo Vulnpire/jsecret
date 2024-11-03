2f1b896d9ffc0fb061fe064967c77a196b2574fd
v1xtron <89225495+Vulnpire@users.noreply.github.com>
2024-11-03 16:30:22 +0100

    Update jsecret.go

--- jsecret.go
+++ jsecret.go
@@ -2,42 +2,30 @@ package main
 
 import (
 	"bufio"
-	"flag"
 	"os"
 	"sync"
 )
 
-var (
-	HashList     = []string{}
-	concurrency  int
-)
-
-func init() {
-	flag.IntVar(&concurrency, "c", 10, "Concurrency level (number of goroutines)")
-}
+var HashList = []string{}
 
 func main() {
-	flag.Parse()
 	var wg sync.WaitGroup
 	scanner := bufio.NewScanner(os.Stdin)
 
-	// Create a buffered channel to limit the number of concurrent goroutines
-	semaphore := make(chan struct{}, concurrency)
-
 	for scanner.Scan() {
+
 		line := scanner.Text()
-		if isUrl(line) {
+		if isUrl(line) == true {
 			wg.Add(1)
-			// Acquire a slot in the semaphore
-			semaphore <- struct{}{}
-			go func(url string) {
+			go func() {
 				defer wg.Done()
-				matcher(url)
-				// Release the slot after the goroutine finishes
-				<-semaphore
-			}(line)
+				matcher(line)
+			}()
+
 		}
+
 	}
 
 	wg.Wait()
+
 }
